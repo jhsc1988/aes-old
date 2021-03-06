fae79525a2d0996646286216b9206684bed5ae66
Jasmin Husić <jhsc1988@gmail.com>
2021-03-06 19:17:22 +0100

    add Pregled računa - HEP - ODS - izvršenje usluge for datatables and server side processing

--- Controllers/RacunOdsIzvrsenjaUslugeController.cs
+++ Controllers/RacunOdsIzvrsenjaUslugeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using aes.Data;
 using aes.Models;
+using System.Linq.Dynamic.Core;
 
 namespace aes.Controllers
 {
@@ -162,5 +163,102 @@ namespace aes.Controllers
         {
             return _context.RacunOdsIzvrsenjaUsluge.Any(e => e.Id == id);
         }
+
+
+
+
+
+        // validation
+        [HttpGet]
+        public async Task<IActionResult> BrojRacunaValidation(string brojRacuna)
+        {
+            if (brojRacuna.Length < 19 || brojRacuna.Length > 19)
+            {
+                return Json($"Broj računa nije ispravan");
+            }
+
+            var db = await _context.RacunElektra.FirstOrDefaultAsync(x => x.BrojRacuna.Equals(brojRacuna));
+            if (db != null)
+            {
+                return Json($"Račun {brojRacuna} već postoji.");
+            }
+            return Json(true);
+        }
+
+
+
+        /// <summary>
+        /// Server side processing - učitavanje, filtriranje, paging, sortiranje podataka iz baze
+        /// </summary>
+        /// <returns>Vraća listu racuna Elektre u JSON obliku za server side processing</returns>
+        [HttpPost]
+        public async Task<IActionResult> GetList()
+        {
+            // server side parameters
+            var start = Request.Form["start"].FirstOrDefault();
+            var length = Request.Form["length"].FirstOrDefault();
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+            var sortColumnName = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+            var sortDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+
+            // async/await - imam overhead (povećavam latency), ali proširujem scalability
+            List<RacunOdsIzvrsenjaUsluge> RacunOdsIzvrsenjaUslugeList = new List<RacunOdsIzvrsenjaUsluge>();
+            RacunOdsIzvrsenjaUslugeList = await _context.RacunOdsIzvrsenjaUsluge.ToListAsync<RacunOdsIzvrsenjaUsluge>();
+
+            // popunjava podatke za JSON da mogu vezane podatke pregledavati u datatables
+            foreach (RacunOdsIzvrsenjaUsluge racunOdsIzvrsenjaUsluge in RacunOdsIzvrsenjaUslugeList)
+            {
+                racunOdsIzvrsenjaUsluge.OdsKupac = await _context.OdsKupac.FirstOrDefaultAsync(o => o.Id == racunOdsIzvrsenjaUsluge.OdsKupacId);
+            }
+
+            // filter
+            // TODO: fali napomena
+            int totalRows = RacunOdsIzvrsenjaUslugeList.Count;
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                RacunOdsIzvrsenjaUslugeList = await RacunOdsIzvrsenjaUslugeList.
+                    Where(
+                    x => x.BrojRacuna.Contains(searchValue)
+                    || x.OdsKupac.SifraKupca.ToString().Contains(searchValue)
+                    || x.DatumIzdavanja.ToString("dd.MM.yyyy").Contains(searchValue)
+                    || x.DatumIzvrsenja.ToString("dd.MM.yyyy").Contains(searchValue)
+                    || (x.Usluga != null && x.Usluga.ToLower().Contains(searchValue.ToLower()))
+                    || x.Iznos.ToString().Contains(searchValue)
+                    || (x.KlasaPlacanja != null && x.KlasaPlacanja.Contains(searchValue))
+                    || (x.DatumPotvrde != null && x.DatumPotvrde.Value.ToString("dd.MM.yyyy").Contains(searchValue))
+                    || (x.Napomena != null && x.Napomena.ToLower().Contains(searchValue))).ToDynamicListAsync<RacunOdsIzvrsenjaUsluge>();
+                    // x.DatumPotvrde.Value mi treba jer metoda nullable objekta ne prima argument za funkciju ToString
+                    // sortiranje radi normalno za datume, neovisno o formatu ToString
+            }
+            int totalRowsAfterFiltering = RacunOdsIzvrsenjaUslugeList.Count;
+
+            // trebam System.Linq.Dynamic.Core;
+            // sorting
+            RacunOdsIzvrsenjaUslugeList = RacunOdsIzvrsenjaUslugeList.AsQueryable().OrderBy(sortColumnName + " " + sortDirection).ToList();
+
+            // paging
+            RacunOdsIzvrsenjaUslugeList = RacunOdsIzvrsenjaUslugeList.Skip(Convert.ToInt32(start)).Take(Convert.ToInt32(length)).ToList<RacunOdsIzvrsenjaUsluge>();
+
+            return Json(new { data = RacunOdsIzvrsenjaUslugeList, draw = Convert.ToInt32(Request.Form["draw"].FirstOrDefault()), recordsTotal = totalRows, recordsFiltered = totalRowsAfterFiltering });
+        }
+
+
+
+
+        // TODO: delete for production  !!!!
+        // Area51
+        [HttpGet]
+        public async Task<IActionResult> GetListJSON()
+        {
+            List<RacunOdsIzvrsenjaUsluge> RacunOdsIzvrsenjaUslugeList = new List<RacunOdsIzvrsenjaUsluge>();
+            RacunOdsIzvrsenjaUslugeList = await _context.RacunOdsIzvrsenjaUsluge.ToListAsync<RacunOdsIzvrsenjaUsluge>();
+
+            foreach (RacunOdsIzvrsenjaUsluge racunOdsIzvrsenjaUsluge in RacunOdsIzvrsenjaUslugeList)
+            {
+                racunOdsIzvrsenjaUsluge.OdsKupac = await _context.OdsKupac.FirstOrDefaultAsync(o => o.Id == racunOdsIzvrsenjaUsluge.OdsKupacId);
+            }
+            return Json(RacunOdsIzvrsenjaUslugeList);
+        }
     }
+
 }
