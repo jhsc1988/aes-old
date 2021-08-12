323311dc785e814fb22a872655c4211012e611b9
Jasmin Husić <jhsc1988@gmail.com>
2021-08-12 19:47:00 +0200

    change home page

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,7 @@ namespace aes
               {
                   _ = endpoints.MapControllerRoute(
                       name: "default",
-                      pattern: "{controller=Home}/{action=Index}/{id?}");
+                      pattern: "{controller=Stanovi}/{action=Index}/{id?}");
                   _ = endpoints.MapRazorPages();
               });
         }
