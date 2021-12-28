9e6e8a8c7694184d25849b4d90a998a7f4027ea2
Jasmin Husić <jhsc1988@gmail.com>
2021-12-28 15:44:08 +0100

    feat: add GetLastForRacunId method to ObracunPotrosnjeRepository

--- Repository/IObracunPotrosnjeRepository.cs
+++ Repository/IObracunPotrosnjeRepository.cs
@@ -7,6 +7,8 @@ namespace aes.Repository
 {
     public interface IObracunPotrosnjeRepository : IRepository<ObracunPotrosnje>
     {
+        Task<ObracunPotrosnje> GetLastForRacunId(int billId);
+        Task<IEnumerable<ObracunPotrosnje>> GetObracunForUgovorniRacun(long ugovorniRacun);
         Task<IEnumerable<ObracunPotrosnje>> GetObracunPotrosnjeForBill(int billId);
     }
 }
--- Repository/ObracunPotrosnjeRepository.cs
+++ Repository/ObracunPotrosnjeRepository.cs
@@ -18,5 +18,41 @@ namespace aes.Repository
                 .Include(e => e.TarifnaStavka)
                 .ToListAsync();
         }
+
+        public async Task<ObracunPotrosnje> GetLastForRacunId(int billId)
+        {
+            if (_context.ObracunPotrosnje.Where(e => e.RacunElektraId == billId).Count() > 1)
+            {
+                ObracunPotrosnje obracun = await _context.ObracunPotrosnje
+                    .Where(e => e.RacunElektraId == billId)
+                    .OrderBy(e => e.Id)
+                    .Reverse()
+                    .Skip(1) // one before last
+                    .FirstOrDefaultAsync();
+
+                obracun.DatumOd = obracun.DatumDo.AddDays(1);
+                obracun.DatumDo = obracun.DatumOd.AddMonths(1);
+                obracun.StanjeOd = obracun.StanjeDo;
+                obracun.StanjeDo = obracun.StanjeOd;
+
+                return obracun;
+            }
+            else
+            {
+                return await _context.ObracunPotrosnje
+                    .Where(e => e.RacunElektraId == billId)
+                    .FirstOrDefaultAsync();
+            }
+        }
+
+        public async Task<IEnumerable<ObracunPotrosnje>> GetObracunForUgovorniRacun(long ugovorniRacun)
+        {
+            return await _context.ObracunPotrosnje
+                .Include(e => e.RacunElektra)
+                .Include(e => e.RacunElektra.ElektraKupac)
+                .Where(e => e.RacunElektra.ElektraKupac.UgovorniRacun == ugovorniRacun)
+                .OrderByDescending(e => e.DatumDo)
+                .ToListAsync();
+        }
     }
 }
