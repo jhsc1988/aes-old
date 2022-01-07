f8841761f17468b2365da83ef49fd57c8ce0d935
Jasmin Husić <jhsc1988@gmail.com>
2022-01-07 10:27:20 +0100

    docs: comment remove

--- Models/Dopis.cs
+++ Models/Dopis.cs
@@ -19,6 +19,6 @@ namespace aes.Models
         public DateTime? Datum { get; set; }
 
         [Display(Name = "Vrijeme unosa")]
-        public DateTime? VrijemeUnosa { get; set; } // nullable mi treba za not required
+        public DateTime? VrijemeUnosa { get; set; }
     }
 }
