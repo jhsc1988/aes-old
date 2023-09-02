384d22cc385b256e886bc49c06850ddc60b4a489
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:28:34 +0200

    usings, renames

--- aes.Repositories/IRepository/HEP/IODSRepository.cs
+++ aes.Repositories/IRepository/HEP/IODSRepository.cs
@@ -1,14 +1,12 @@
 ﻿using aes.Models.HEP;
 using aes.Models.Racuni.Elektra;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace aes.Repository.IRepository.HEP
 {
-    public interface IODSRepository : IRepository<Ods>
+    public interface IOdsRepository : IRepository<Ods>
     {
         Task<IEnumerable<Ods>> GetAllOds();
         Task<IEnumerable<TRacun>> GetRacuniForOmm<TRacun>(int stanId) where TRacun : Elektra;
-        Task<Ods> IncludeAppartment(Ods ods);
+        Task<Ods> IncludeApartment(Ods ods);
     }
 }
