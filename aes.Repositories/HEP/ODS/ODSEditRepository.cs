6ebc9c87faf3245342f58ae6391af8777edec96a
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:22:42 +0200

    renames, nullable

--- aes.Repositories/HEP/ODS/ODSEditRepository.cs
+++ aes.Repositories/HEP/ODS/ODSEditRepository.cs
@@ -2,17 +2,15 @@
 using aes.Models.HEP;
 using aes.Repository.IRepository.HEP;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace aes.Repository.HEP.ODS
 {
     public class OdsEditRepository : Repository<OdsEdit>, IOdsEditRepository
     {
         public OdsEditRepository(ApplicationDbContext context) : base(context) { }
-        public async Task<OdsEdit> GetLastOdsEdit(string userId)
+        public async Task<OdsEdit?> GetLastOdsEdit(string userId)
         {
-            return await _context.OdsEdit
+            return await Context.OdsEdit
                 .Include(e => e.Ods)
                 .Where(e => e.EditingByUserId == userId)
                 .OrderByDescending(e => e.EditTime)
