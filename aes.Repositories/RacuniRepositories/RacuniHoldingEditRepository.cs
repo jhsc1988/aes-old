830c0ab11faf7e2e5dacf7a674355019421c31dc
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:14:20 +0200

    usings

--- aes.Repositories/RacuniRepositories/RacuniHoldingEditRepository.cs
+++ aes.Repositories/RacuniRepositories/RacuniHoldingEditRepository.cs
@@ -2,22 +2,19 @@
 using aes.Models.Racuni.Holding;
 using aes.Repository.RacuniRepositories.IRacuniRepository;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace aes.Repository.RacuniRepositories
+namespace aes.Repository.RacuniRepositories;
+
+public class RacuniHoldingEditRepository : Repository<RacunHoldingEdit>, IRacuniHoldingEditRepository
 {
-    public class RacuniHoldingEditRepository : Repository<RacunHoldingEdit>, IRacuniHoldingEditRepository
-    {
-        public RacuniHoldingEditRepository(ApplicationDbContext context) : base(context) { }
+    public RacuniHoldingEditRepository(ApplicationDbContext context) : base(context) { }
 
-        public async Task<RacunHoldingEdit> GetLastRacunHoldingEdit(string userId)
-        {
-            return await _context.RacunHoldingEdit
-                .Include(e => e.RacunHolding)
-                .Where(e => e.EditingByUserId == userId)
-                .OrderByDescending(e => e.EditTime)
-                .FirstOrDefaultAsync();
-        }
+    public async Task<RacunHoldingEdit> GetLastRacunHoldingEdit(string userId)
+    {
+        return await Context.RacunHoldingEdit
+            .Include(e => e.RacunHolding)
+            .Where(e => e.EditingByUserId == userId)
+            .OrderByDescending(e => e.EditTime)
+            .FirstOrDefaultAsync();
     }
 }
