ac365bddcd05d05ad280b0582cc2121c127cbc64
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:11:09 +0200

    usings, formatting

--- aes.Repositories/IRepository/IPredmetRepository.cs
+++ aes.Repositories/IRepository/IPredmetRepository.cs
@@ -1,11 +1,10 @@
 ﻿using aes.Models;
 using aes.Models.Racuni;
-using System.Collections.Generic;
 
 namespace aes.Repository.IRepository
 {
     public interface IPredmetRepository : IRepository<Predmet>
     {
-        IEnumerable<Predmet> GetPredmetfForAllPayedRacuni(IEnumerable<Racun> Racuni);
+        IEnumerable<Predmet> GetPredmetForAllPaidRacuni(IEnumerable<Racun> racuni);
     }
 }
