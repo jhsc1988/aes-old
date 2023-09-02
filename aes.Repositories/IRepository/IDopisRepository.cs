5e7aa458dd24e6f5e927af57a1a3c9df174d7059
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:25:28 +0200

    usings

--- aes.Repositories/IRepository/IDopisRepository.cs
+++ aes.Repositories/IRepository/IDopisRepository.cs
@@ -1,13 +1,11 @@
 ﻿using aes.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace aes.Repository.IRepository
 {
     public interface IDopisRepository : IRepository<Dopis>
     {
         Task<IEnumerable<Dopis>> GetDopisiForPredmet(int predmetId);
-        Task<IEnumerable<Dopis>> GetOnlyEmptyDopisi(int predmetId);
-        Task<Dopis> IncludePredmet(Dopis Dopis);
+        Task<IEnumerable<Dopis>> GetOnlyEmptyDopisiAsync(int predmetId);
+        Task<Dopis> IncludePredmetAsync(Dopis dopis);
     }
 }
