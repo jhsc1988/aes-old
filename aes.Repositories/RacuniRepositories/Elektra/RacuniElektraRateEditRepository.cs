d8f486bcb13fceaee63c5b388a3f06a8cef76ba1
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:16:11 +0200

    formatting

--- aes.Repositories/RacuniRepositories/Elektra/RacuniElektraRateEditRepository.cs
+++ aes.Repositories/RacuniRepositories/Elektra/RacuniElektraRateEditRepository.cs
@@ -5,19 +5,18 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace aes.Repository.RacuniRepositories.Elektra
-{
-    public class RacuniElektraRateEditRepository : Repository<RacunElektraRateEdit>, IRacuniElektraRateEditRepository
-    {
-        public RacuniElektraRateEditRepository(ApplicationDbContext context) : base(context) { }
+namespace aes.Repository.RacuniRepositories.Elektra;
 
-        public async Task<RacunElektraRateEdit> GetLastRacunElektraRateEdit(string userId)
-        {
-            return await _context.RacunElektraRateEdit
-                .Include(e => e.RacunElektraRate)
-                .Where(e => e.EditingByUserId == userId)
-                .OrderByDescending(e => e.EditTime)
-                .FirstOrDefaultAsync();
-        }
+public class RacuniElektraRateEditRepository : Repository<RacunElektraRateEdit>, IRacuniElektraRateEditRepository
+{
+    public RacuniElektraRateEditRepository(ApplicationDbContext context) : base(context) { }
+
+    public async Task<RacunElektraRateEdit> GetLastRacunElektraRateEdit(string userId)
+    {
+        return await Context.RacunElektraRateEdit
+            .Include(e => e.RacunElektraRate)
+            .Where(e => e.EditingByUserId == userId)
+            .OrderByDescending(e => e.EditTime)
+            .FirstOrDefaultAsync();
     }
 }
