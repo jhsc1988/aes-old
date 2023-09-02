af743431aabafc20912f52bc60988011906fd377
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 19:11:20 +0200

    formatting

--- aes.Repositories/RacuniRepositories/IRacuniRepository/Elektra/IRacuniElektraIzvrsenjeUslugeEditRepository.cs
+++ aes.Repositories/RacuniRepositories/IRacuniRepository/Elektra/IRacuniElektraIzvrsenjeUslugeEditRepository.cs
@@ -1,11 +1,9 @@
 ﻿using aes.Models.Racuni.Elektra;
 using aes.Repository.IRepository;
-using System.Threading.Tasks;
 
-namespace aes.Repository.RacuniRepositories.IRacuniRepository.Elektra
+namespace aes.Repository.RacuniRepositories.IRacuniRepository.Elektra;
+
+public interface IRacuniElektraIzvrsenjeUslugeEditRepository : IRepository<RacunElektraIzvrsenjeUslugeEdit>
 {
-    public interface IRacuniElektraIzvrsenjeUslugeEditRepository : IRepository<RacunElektraIzvrsenjeUslugeEdit>
-    {
-        Task<RacunElektraIzvrsenjeUslugeEdit> GetLastRacunElektraServiceEdit(string userId);
-    }
+    Task<RacunElektraIzvrsenjeUslugeEdit> GetLastRacunElektraServiceEdit(string userId);
 }
