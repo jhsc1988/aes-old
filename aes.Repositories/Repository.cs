7342ef1b4101c734c3bc0e9cf40b0773420a754f
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-02 18:56:03 +0200

    renames, asyncs, usings

--- aes.Repositories/Repository.cs
+++ aes.Repositories/Repository.cs
@@ -1,74 +1,69 @@
 ﻿using aes.Data;
 using aes.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace aes.Repository
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        protected readonly ApplicationDbContext _context;
+        protected readonly ApplicationDbContext Context;
 
-        public Repository(ApplicationDbContext context)
+        protected Repository(ApplicationDbContext context)
         {
-            _context = context;
+            Context = context;
         }
 
         public async Task<TEntity> Get(int id)
         {
-            return await _context.Set<TEntity>().FindAsync(id);
+            return await Context.Set<TEntity>().FindAsync(id);
         }
 
         public async Task<IEnumerable<TEntity>> GetAll()
         {
-            return await _context.Set<TEntity>().ToListAsync();
+            return await Context.Set<TEntity>().ToListAsync();
         }
 
         public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
         {
-            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+            return await Context.Set<TEntity>().Where(predicate).ToListAsync();
         }
 
-
-        public async void Add(TEntity entitity)
+        public async Task Add(TEntity entity)
         {
-            _ = await _context.Set<TEntity>().AddAsync(entitity);
+            await Context.Set<TEntity>().AddAsync(entity);
         }
 
-        public async void AddRange(IEnumerable<TEntity> entities)
+        public async Task AddRange(IEnumerable<TEntity> entities)
         {
-            await _context.Set<TEntity>().AddRangeAsync(entities);
+            await Context.Set<TEntity>().AddRangeAsync(entities);
         }
-
 
         public void Remove(TEntity entity)
         {
-            _ = _context.Set<TEntity>().Remove(entity);
+            Context.Set<TEntity>().Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<TEntity> entities)
         {
-            _context.Set<TEntity>().RemoveRange(entities);
+            Context.Set<TEntity>().RemoveRange(entities);
         }
 
         public async Task<int> Update(TEntity entity)
         {
             // In case AsNoTracking is used
-            _context.Entry(entity).State = EntityState.Modified;
-            return await _context.SaveChangesAsync();
+            Context.Entry(entity).State = EntityState.Modified;
+            return await Context.SaveChangesAsync();
         }
 
-        public bool Any(Expression<Func<TEntity, bool>> predicate)
+        public async Task<bool> Any(Expression<Func<TEntity, bool>> predicate)
         {
-            return _context.Set<TEntity>().Any(predicate);
+            return await Context.Set<TEntity>().AnyAsync(predicate);
         }
+
         public async Task<TEntity> FindExact(Expression<Func<TEntity, bool>> predicate)
         {
-            return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            return await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
         }
     }
 }
