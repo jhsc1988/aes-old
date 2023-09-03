38ac5cbda9bbf963738004ef07a3036b29022303
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2023-09-03 11:03:01 +0200

    add precompiled query

--- aes.Repositories/Stan/StanRepository.cs
+++ aes.Repositories/Stan/StanRepository.cs
@@ -1,25 +1,33 @@
 ﻿using aes.Data;
 using aes.Repository.IRepository;
-using aes.Repository.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using aes.UnitOfWork;
 
 namespace aes.Repository.Stan
 {
     public class StanRepository : Repository<Models.Stan>, IStanRepository
     {
         private readonly IUnitOfWork _unitOfWork;
+        public new ApplicationDbContext Context { get; }
+
+        private static readonly Func<ApplicationDbContext, int, IEnumerable<Models.Stan>> CompiledQuery 
+            = EF.CompileQuery((ApplicationDbContext dbContext, int id) => dbContext.Stan.Where(e => e.Id != id));
 
         public StanRepository(ApplicationDbContext context, IUnitOfWork unitOfWork) : base(context)
         {
+            Context = context;
             _unitOfWork = unitOfWork;
         }
 
         public async Task<IEnumerable<Models.Stan>> GetStanovi()
         {
             // HACK: dummy entity
-            return await _unitOfWork.Stan.Find(e => e.Id != 25265);
+            // return await _unitOfWork.Stan.Find(e => e.Id != 25265);
+            var dbContext = _unitOfWork.Stan.Context;
+            var result = CompiledQuery(dbContext, 25265).ToList();
+            return await Task.FromResult(result);
         }
 
         public async Task<IEnumerable<Models.Stan>> GetStanoviWithoutOdsOmm()
